4f1a830b647501aaed9d2d7708a16bb7cd6e4e90
Ulf Karlsson <karlsson.ulf@gmail.com>
2018-03-05 23:16:25 +0100

    Use toolchain

---
+++ Toolchain.cmake
@@ -0,0 +1,8 @@
+set(WASM_INSTALL_DIR $ENV{HOME}/opt/wasm)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_CXX_COMPILER
+        ${WASM_INSTALL_DIR}/bin/clang++ --target=wasm32)
+set(CMAKE_CXX_CREATE_SHARED_LIBRARY
+        "${WASM_INSTALL_DIR}/bin/llvm-link -o <TARGET> <OBJECTS>")
