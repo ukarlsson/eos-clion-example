2aef63caabc40ebfc5fd05f2132e2da2a1665207
Ulf Karlsson <karlsson.ulf@gmail.com>
2018-04-05 21:46:05 +0200

    Update for Dawn 3.x

--- Toolchain.cmake
+++ Toolchain.cmake
@@ -5,4 +5,4 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_CXX_COMPILER
         ${WASM_INSTALL_DIR}/bin/clang++ --target=wasm32)
 set(CMAKE_CXX_CREATE_SHARED_LIBRARY
-        "${WASM_INSTALL_DIR}/bin/llvm-link -o <TARGET> <OBJECTS>")
+        "${WASM_INSTALL_DIR}/bin/llvm-link -only-needed -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
